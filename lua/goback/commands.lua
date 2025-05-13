2d8cd37775e0fa3bd5b30794fb496180c8ff7a81
howardng97 <longtn@xgamevn.com>
2025-05-13 10:47:00 +0700

    text window buffer using split window

--- lua/goback/commands.lua
+++ lua/goback/commands.lua
@@ -3,62 +3,87 @@ local M = {}
 
 -- Safely set the buffer if it is valid
 local function safe_set_buf(bufnr)
-	if vim.api.nvim_buf_is_valid(bufnr) then
-		vim.api.nvim_set_current_buf(bufnr)
-	else
-		print("Buffer does not exist!")
-	end
+  if vim.api.nvim_buf_is_valid(bufnr) then
+    vim.api.nvim_set_current_buf(bufnr)
+  else
+    print("Buffer does not exist!")
+  end
 end
 
 -- Safely set the tab if it is valid
 local function safe_set_tab(tabnr)
-	if vim.api.nvim_tabpage_is_valid(tabnr) then
-		vim.api.nvim_set_current_tabpage(tabnr)
-	else
-		print("Tab does not exist!")
-	end
+  if vim.api.nvim_tabpage_is_valid(tabnr) then
+    vim.api.nvim_set_current_tabpage(tabnr)
+  else
+    print("Tab does not exist!")
+  end
 end
 
--- Return to the last saved position
+-- Check if in split mode
+local function is_split_mode()
+  return vim.fn.winnr("$") > 1
+end
+
+-- Find the window that contains the last buffer
+local function find_window_with_buf(bufnr)
+  for _, win_id in ipairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_is_valid(win_id) and vim.api.nvim_win_get_buf(win_id) == bufnr then
+      return win_id
+    end
+  end
+  return nil
+end
+
 function M.return_last_leave(S, config)
-	if not S["last"] then
-		print("No previous position to return to!")
-		return
-	end
+  if not S["last"] then
+    print("No previous position to return to!")
+    return
+  end
 
-	-- Set flag
-	S.is_returning = true
+  -- Set flag to prevent recursive save
+  S.is_returning = true
 
-	-- Switch to the saved tab if configured
-	if config.save_tab and S["last"].tab then
-		safe_set_tab(S["last"].tab)
-	end
+  -- Switch to the saved tab if configured
+  if config.save_tab and S["last"].tab then
+    safe_set_tab(S["last"].tab)
+  end
 
-	-- Switch to the saved buffer
-	safe_set_buf(S["last"].buf)
+  -- Check if in split mode and find the window containing the last buffer
+  local win_id = nil
+  if is_split_mode() then
+    win_id = find_window_with_buf(S["last"].buf)
+  end
 
-	-- Switch to the saved window if configured
-	local win_id = vim.api.nvim_get_current_win()
-	if config.save_win and S["last"].win then
-		win_id = S["last"].win
-	end
+  -- If window not found or not in split mode, use the saved window ID
+  if not win_id and config.save_win and S["last"].win then
+    win_id = S["last"].win
+  end
 
-	-- Restore cursor position
-	if S["last"].buf_cursor then
-		vim.api.nvim_win_set_cursor(win_id, S["last"].buf_cursor)
-	else
-		print("Unable to set the cursor!")
-	end
+  -- If window is valid, set it as the current window
+  if win_id and vim.api.nvim_win_is_valid(win_id) then
+    vim.api.nvim_set_current_win(win_id)
+  else
+    print("Window does not exist or is not valid!")
+    -- Fallback: Open the buffer in the current window
+    safe_set_buf(S["last"].buf)
+  end
 
-	-- Unset the flag
-	S.is_returning = false
+  -- Restore cursor position
+  if S["last"].buf_cursor then
+    vim.api.nvim_win_set_cursor(win_id or 0, S["last"].buf_cursor)
+  else
+    print("Unable to set the cursor!")
+  end
+
+  -- Unset the flag after completion
+  S.is_returning = false
 end
 
 -- Register the GoBackLast user command
 function M.setup_user_commands(S, config)
-	vim.api.nvim_create_user_command("GoBackLast", function()
-		M.return_last_leave(S, config)
-	end, {})
+  vim.api.nvim_create_user_command("GoBackLast", function()
+    M.return_last_leave(S, config)
+  end, {})
 end
 
 return M
