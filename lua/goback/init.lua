fe46d49bb31c164560d32e5d36ad3c6b888facb5
howardng97 <longtn@xgamevn.com>
2025-05-13 09:51:38 +0700

    fix add callback for neo-tree file switching

--- lua/goback/init.lua
+++ lua/goback/init.lua
@@ -1,8 +1,8 @@
 -- lua/goback/init.lua
 local M = {}
 local config = {
-	save_tab = true,
-	save_win = true,
+  save_tab = true,
+  save_win = true,
 }
 
 -- State to save last position
@@ -10,55 +10,55 @@ local S = {}
 
 -- Save the current position
 local function on_leave()
-	local bufnr = vim.api.nvim_get_current_buf()
-	local buf_cursor = vim.api.nvim_win_get_cursor(0)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local buf_cursor = vim.api.nvim_win_get_cursor(0)
 
-	-- Create a new save point
-	S["last"] = { buf = bufnr, buf_cursor = buf_cursor }
+  -- Create a new save point
+  S["last"] = { buf = bufnr, buf_cursor = buf_cursor }
 
-	-- Save tab if configured
-	if M.config.save_tab then
-		local tabnr = vim.api.nvim_get_current_tabpage()
-		S["last"].tab = tabnr
-	end
+  -- Save tab if configured
+  if M.config.save_tab then
+    local tabnr = vim.api.nvim_get_current_tabpage()
+    S["last"].tab = tabnr
+  end
 
-	-- Save window if configured
-	if M.config.save_win then
-		local winnr = vim.api.nvim_get_current_win()
-		S["last"].win = winnr
-	end
+  -- Save window if configured
+  if M.config.save_win then
+    local winnr = vim.api.nvim_get_current_win()
+    S["last"].win = winnr
+  end
 end
 
 -- Setup function and event registration
 function M.setup(opts)
-	opts = opts or {}
-	M.config = vim.tbl_deep_extend("force", config, opts)
+  opts = opts or {}
+  M.config = vim.tbl_deep_extend("force", config, opts)
 
-	-- Create autocommands for buffer and window events
-	vim.api.nvim_create_autocmd({ "BufLeave", "BufWinLeave" }, {
-		pattern = "*",
-		callback = on_leave,
-	})
+  -- Create autocommands for buffer and window events
+  vim.api.nvim_create_autocmd({ "BufLeave", "BufWinLeave", "BufEnter", "WinEnter", "TabEnter" }, {
+    pattern = "*",
+    callback = on_leave,
+  })
 
-	if M.config.save_win then
-		vim.api.nvim_create_autocmd("WinLeave", {
-			pattern = "*",
-			callback = on_leave,
-		})
-	end
+  if M.config.save_win then
+    vim.api.nvim_create_autocmd("WinLeave", {
+      pattern = "*",
+      callback = on_leave,
+    })
+  end
 
-	if M.config.save_tab then
-		vim.api.nvim_create_autocmd("TabLeave", {
-			pattern = "*",
-			callback = on_leave,
-		})
-	end
+  if M.config.save_tab then
+    vim.api.nvim_create_autocmd("TabLeave", {
+      pattern = "*",
+      callback = on_leave,
+    })
+  end
 
-	-- Import and setup user commands
-	local commands = require("goback.commands")
-	commands.setup_user_commands(S, M.config)
+  -- Import and setup user commands
+  local commands = require("goback.commands")
+  commands.setup_user_commands(S, M.config)
 
-	print("GoBackLast plugin loaded!")
+  print("GoBackLast plugin loaded!")
 end
 
 return M
